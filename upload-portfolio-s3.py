b9a03adcf002a9c0f9cca6b233aa5b615cbdd646
Moayad Ismail <moayad.ismail@boundarynetworks.com.au>
2017-09-24 22:00:12 +1000

    update to buildspec

--- upload-portfolio-s3.py
+++ upload-portfolio-s3.py
@@ -3,6 +3,7 @@ import zipfile
 import StringIO
 import mimetypes
 
+
 s3 = boto3.resource('s3')
 
 portfolio_bucket = s3.Bucket('portfolio.aws.boundarynetworks.com.au')
@@ -15,6 +16,8 @@ build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 with zipfile.ZipFile(portfolio_zip) as myzip:
     for nm in myzip.namelist():
         obj = myzip.open(nm)
+        mime_type=mimetypes.guess_type(nm)[0]
+        print mime_type
         portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
