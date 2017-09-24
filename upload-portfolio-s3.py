9a79239966b64e5ff974217b6bb7bfa00094c7b9
Moayad Ismail <moayad.ismail@boundarynetworks.com.au>
2017-09-24 22:35:08 +1000

    update to match the lambda function updates

--- upload-portfolio-s3.py
+++ upload-portfolio-s3.py
@@ -4,20 +4,24 @@ import StringIO
 import mimetypes
 
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
 
-portfolio_bucket = s3.Bucket('portfolio.aws.boundarynetworks.com.au')
-build_bucket = s3.Bucket('portfoliobuild.aws.boundarynetworks.com.au')
+    portfolio_bucket = s3.Bucket('portfolio.aws.boundarynetworks.com.au')
+    build_bucket = s3.Bucket('portfoliobuild.aws.boundarynetworks.com.au')
 
-portfolio_zip = StringIO.StringIO()
+    portfolio_zip = StringIO.StringIO()
 
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        mime_type=mimetypes.guess_type(nm)[0]
-        print mime_type
-        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            mime_type = mimetypes.guess_type(nm)[0]
+            print mime_type
+            portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return "Job Complete!"
+
 
